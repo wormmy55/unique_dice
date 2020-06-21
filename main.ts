881285a3e4cb1d86bd3c5867a73e8cb801a1d25b
wormmy55 <67141048+wormmy55@users.noreply.github.com>
2020-06-21 12:25:16 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,103 @@
+input.onButtonPressed(Button.A, function () {
+    random = randint(1, 12)
+    if (random == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    } else if (random == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . # #
+            . . . # #
+            `)
+    } else if (random == 3) {
+        basic.showLeds(`
+            # # . # #
+            # . . . #
+            . . . . .
+            . . # . .
+            . # # # .
+            `)
+    } else if (random == 4) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (random == 5) {
+        basic.showLeds(`
+            # # . # #
+            # . . . #
+            . . # . .
+            # . . . #
+            # # . # #
+            `)
+    } else if (random == 6) {
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            # # . # #
+            . . . . .
+            # # . # #
+            `)
+    } else if (random == 7) {
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            # . # . #
+            . . . . .
+            # # . # #
+            `)
+    } else if (random == 8) {
+        basic.showLeds(`
+            # . . . #
+            . . # . .
+            # . . . #
+            . . # . .
+            # . . . #
+            `)
+    } else if (random == 9) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            `)
+    } else if (random == 10) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . . . #
+            `)
+    } else if (random == 11) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . # . #
+            `)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     random = randint(1, 6)
     if (random == 1) {
